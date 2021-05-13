d373e1ee9a35f2637efdad275f7f3001da3dabe6
Abu Raihan Rony <44436797+ronyaburaihan@users.noreply.github.com>
2021-05-13 12:00:25 +0600

    Navigation bar added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,42 @@
+/*--------------------- Navbar Toggler -----------------------*/
+
+const navToggler = document.querySelector('.nav-toggler')
+
+navToggler.addEventListener('click', () => {
+    hideSection()
+    toggleNavbar()
+    document.body.classList.toggle('hide-scrolling')
+})
+
+function hideSection() {
+    document.querySelector('section.active').classList.toggle('fade-out')
+}
+
+function toggleNavbar() {
+    document.querySelector('.header').classList.toggle('active')
+}
+
+/*--------------------- Active section -----------------------*/
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('link-item') && e.target.hash !== '') {
+
+        //navToggler.classList.add('hide')
+
+        if (e.target.classList.contains('nav-item')) {
+            toggleNavbar()
+        } else {
+            hideSection()
+            document.body.classList.add('hide-scrolling')
+        }
+
+        document.querySelector('section.active').classList.remove('active', 'fade-out')
+        document.querySelector(e.target.hash).classList.add('active')
+        window.scrollTo(0, 0)
+        document.body.classList.remove('hide-scrolling')
+        //navToggler.classList.remove('hide')
+    }
+})
+
 /*--------------------- About section -----------------------*/
 const tabsContainer = document.querySelector('.about-tabs'),
     aboutSection = document.querySelector('.about-section')
